57b5728646fc615ec97dc8dd572f8a4b2a1831e2
root <root@instance-2.us-central1-a.c.my-project-1535475528695.internal>
2019-06-21 16:09:17 +0000

    add enter keypress event

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const link = document.getElementById('link');
+const input = document.getElementById('input');
 const opener = document.getElementById('opener');
 
-const openWindowApp = () => window.open(link.value, link.value, "location=no,resizable=yes,toolbar=no,scrollbars=no,menubar=no,status=no,directories=no,width=1200,height=600,left=100,top=100");
+const openWindowApp = () => window.open(input.value, input.value, "location=no,resizable=yes,toolbar=no,scrollbars=no,menubar=no,status=no,directories=no,width=1200,height=600,left=100,top=100");
 
 opener.addEventListener('click', openWindowApp);
+input.addEventListener('keypress', (e) => {
+    const key = 'which' in e ? e.which : e.keyCode;
+    if (key === 13){
+        openWindowApp()
+    }
+})
